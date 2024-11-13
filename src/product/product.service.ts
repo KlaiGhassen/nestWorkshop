8c705cde46d002aebe3fffe3db1807451a8c1698
KlaiGhassen <ghassen.klai@esprit.tn>
2024-11-13 17:55:06 +0100

    first commit

---
+++ src/product/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './entities/product.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+  ) {}
+
+  create(createProductDto: CreateProductDto) {
+    return this.productModel.create(createProductDto);
+  }
+
+  findAll() {
+    return this.productModel.find();
+  }
+
+  findOne(id: string) {
+    return this.productModel.findById(id);
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+   
+
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
